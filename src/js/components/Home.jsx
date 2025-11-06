eb416d235c751d1cc347a3c08c886ec1897518d0
Jonathan Palma <166739509+gitttjonzzz@users.noreply.github.com>
2025-11-06 02:11:27 +0000

    API

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,86 +1,102 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import axios from 'axios'
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-const [tasks, setTasks] =React.useState([{label:"Run 3 miles",isDone:false},{label:"Shower", isDone:false},{label:"Study",isDone:false}])
-const[userInput, setUserInput]=React.useState("")
+	const [tasks, setTasks] = React.useState([])
+	const [userInput, setUserInput] = React.useState("")
 
+	useEffect(() => {
+		getUser()
+	}, [])
 
-const newTask=(event)=>{
-	
-	if (event.key === 'Enter'){
-		// Check if input is empty FIRST, before adding task
-		if (userInput.trim() === "") {
-			alert("You must type something in! (e.g. Eat BreakFast)")
-			event.preventDefault()
-			return  // Stop here, don't add the task
-		}
-		
-		// Only runs if validation passes
-		let newTask = {label:userInput, isDone: false}
-		setTasks([...tasks, newTask])
-		setUserInput("")
-		event.preventDefault()
+	const getUser = () => {
+		axios.get("https://playground.4geeks.com/todo/users/jpalma13")
+			.then((response) => {
+				console.log(response)
+				setTasks(response.data.todos)
+			}).catch(error => {
+				axios.post("https://playground.4geeks.com/todo/users/jpalma13").then(postResponse => postResponse.data)
+					.then(postData => { })
+			})
 	}
-}
-	
 
-const enterKey=(event)=>{
-	if (event.key === 'Enter'){
-		if (!userInput || userInput === "") {
-			alert("You must type something in! (e.g. Eat BreakFast)")
-			event.preventDefault()
-			return  
-		}else{
-			let newTask = {label:userInput, isDone: false}
-			setTasks([...tasks, newTask])
-			setUserInput("")
-			event.preventDefault()
+	const newTask = () => {
+		if (userInput === "") {
+			return alert("You must type something in! (e.g. Eat BreakFast)")
 		}
+		let newTask = { label: userInput, isDone: false }
+		axios.post("https://playground.4geeks.com/todo/todos/jpalma13", newTask).then(postResponse => postResponse.data)
+			.then(postData => {
+				setUserInput("")
+				getUser()
+				console.log(postData)
+			});
+
 	}
-}
 
-const Remove=(index)=>{
-	setTasks(tasks.filter((task, i) => i!== index))
-}
-const toggleCheckmark=(i)=>{
-	let updatedTask= tasks.filter((task, index)=>{
-		if (index ===i){
-			return {...tasks, isDone:!task.isDone}  
-		}else{
-			return tasks
+	const enterKey = (event) => {
+		if (event.key === 'Enter') {
+				newTask()
+				event.preventDefault()
+			}
 		}
-	})
-	setTasks(updatedTask)
+
+			
+const deleteTask = (id) => {
+    			axios.delete("https://playground.4geeks.com/todo/todos/" + id)
+        			.then(deleteResponse => deleteResponse.data)
+       				 .then(deleteData => {
+						getUser()
+        });
+	}
+	
+const putRequest = (id, updatedTask) => {
+    axios.put("https://playground.4geeks.com/todo/todos/" + id, updatedTask)
+        .then(putResponse => putResponse.data)
+        .then(putData => {
+            getUser() 
+            console.log(putData)
+        });
 }
+	const toggleCheckmark = (index) => {
+		let updatedTask = tasks.filter((task, index) => {
+			if (index === i) {
+				return { ...task, isDone: !task.isDone }
+			} else {
+				return task
+			}
+		})
+		setTasks(updatedTask)
+	}
+
 
 	return (
 		<div className="container">
 			<h3>TO-DO List</h3>
-			<div input-container>
+			<div className="input-container">
 				<input className="text-input" type="text" placeholder="Enter task here ..."
-				onChange={(e) => setUserInput(e.target.value)}
-				value={userInput}
-				onKeyDown={enterKey}/>
+					onChange={(e) => setUserInput(e.target.value)}
+					value={userInput}
+					onKeyDown={enterKey} />
 				<button className="add" onClick={newTask}>Add Task</button>
 			</div>
 			<div>
 				<ul className="lists">
 					{tasks?.map((task, index) =>
 						<li key={index}>
-							<input className="checked" type="checkbox" onChange={ ()=>toggleCheckmark(index)} checked={tasks.isDone} />
+							<input className="checked" type="checkbox" onChange={() => toggleCheckmark(index)} checked={task.isDone} />
 							<label>{task.label}</label>
-							<button className="remove" onClick={()=>Remove(index)}>🗑️</button>	
+							<button className="remove" onClick={() => deleteTask(task.id)}>🗑️</button>
 						</li>
 					)}
 				</ul>
 			</div>
 		</div>
-		
+
 	);
-};                  
+};
 
 export default Home;
